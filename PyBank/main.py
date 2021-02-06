378ff89c885ce9b9aba511642efe2c8ffd889deb
Jessica Dean <thething123789@yahoo.com>
2021-02-06 09:39:34 -0600

    fort to add amount

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,9 +4,9 @@ total_months = 0
 total_profit_loss_amount = 0.00
 
 average_profit_loss = 0.00
-greatest_increase = {}
+greatest_increase = {"date":"", "amount": 0}
 
-greatest_decrease = {}
+greatest_decrease = {"date":"", "amount": 0}
 
 
 import csv
@@ -23,11 +23,18 @@ with open(file_path) as csvfile:
     for row in csvreader:
         #The total number of months included in the dataset
         total_months = total_months + 1
+        date= row[0]
+        profit = float(row[1])
        #The net total amount of "Profit/Losses" over the entire period
         #Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
+        if (profit > greatest_increase["amount"]):
+            greatest_increase["date"] = date
+            greatest_increase["amount"] = profit
         #The greatest increase in profits (date and amount) over the entire period
         #The greatest decrease in losses (date and amount) over the entire period
-
+        if (profit < greatest_decrease["amount"]):
+            greatest_decrease["date"] = date
+            greatest_decrease["amount"] = profit
 
 
 
@@ -35,7 +42,7 @@ with open(file_path) as csvfile:
 print("Financial Analysis")
 print("----------------------------")
 print(f"Total Months: {total_months}")
-
+print(f"Greatest Increase in Profits: {greatest_increase['date']} (${greatest_increase['amount']})")
 
 
 # results should look like
