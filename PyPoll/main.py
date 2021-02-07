d5686215196aeb3c7410dd3f7233549885307ce3
Jessica Dean <thething123789@yahoo.com>
2021-02-07 09:22:57 -0600

    added base data

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,3 +1,23 @@
+import csv
+import os
+
+
+file_path = "./Resources/budget_data.csv"
+out_file="./Analysis/output.txt"
+
+
+v_count = []
+v_percent = []
+cand_list = []
+diffrent_cand = []
+count = []
+
+
+
+
+
+
+
 #The total number of votes cast
 
 #A complete list of candidates who received votes
