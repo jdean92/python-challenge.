0e89693ae935fe6a0b484e9b79d665efa19f510c
Jessica Dean <thething123789@yahoo.com>
2021-02-08 17:25:01 -0600

    added u and can list

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -12,9 +12,9 @@ county = []
 v_count = 0
 count = 0
 v_percent = 0
-cand_list = {}
+cand_list = {"Candidate": [], "Votes": [], "Vote Perc": []}
 winner_cand = 0
-vote_won = 0
+
 
 with open(file_path) as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
@@ -23,55 +23,69 @@ with open(file_path) as csvfile:
     csv_header = next(csvreader)
 
     for row in csvreader:
+
+        count = count +1
+    for u in cand_list["Candidate"]:
+        index = cand_list["Candidate"].index(u)
+        file_path.write(f"{u}: {cand_list['Vote Perc'][indx]}% ({cand_list['Votes'][indx]})\n")
+
         #The total number of months included in the dataset
         #voter_id = voter_id + 1
-        count = count +1
+        
         #cand_list.append (row[2])
 
-    for w in cand_list:
-        #if any ([True for k,v in cand_list.items() ]):
-        if w in cand_list:
-            v_count = cand_list[w[2]]
-            cand_list[w[2]] = v_count +1
+    # for w in cand_list:
 
-        else:  
-            cand_list[w[2]] = 1
+        #if any ([True for k,v in cand_list.items() ]):
+        # if w in cand_list:
+        #     v_count = cand_list[w[2]]
+        #     cand_list[w[2]] = v_count +1
+
+        # else:  
+        #     cand_list[w[2]] = 1
 
 
             
-        if voter_can > winner_cand:
-            winner_cand = v_count
+        # if voter_can > winner_cand:
+        #     winner_cand = v_count
 
-            winner_cand.append(w)
-            c = cand_list.count(w)
-            v_count.append(c)
-            d = (c/count)*100
+        #     winner_cand.append(w)
+        #     c = cand_list.count(w)
+        #     v_count.append(c)
+        #     d = (c/count)*100
+for i in len(cand_list):
+    print (f"{cand_list[i]}: {v_percent}% ")
 
 
 
+# print(f"Total Votes: {count}")
+# print(f"increased profits {maxum_date} {increase_profit}")
+# print(f"decreased profits {decrease_date} {decrease_profit}")
+# print(f"avgerage change:{round(avgerage_change,2)}")
+# print(f"total profit:{total_profit}")
 
-#print("Election Results")
+print("Election Results")
 
-#print("--------------------------")
+print("--------------------------")
 
 #print(f" Total Votes: {}") 
 
-#print("--------------------------")
+print("--------------------------")
 
 #for name, v_count in v_count.items():
     
-    #print(f"{name}: {perc_of_candidate[name]} ({v_count})")
+#print(f"{winner_cand}: {v_percent[winner_cand]} ({v_count})")
     
 #print("--------------------------")
 
-#print(f"AND the Winner is: {vote_won}!")
+#print(f"AND the Winner is: {v_count}!")
 
 #print("--------------------------")
 
 
 
 
-#print(f"vote won: {vote_won}")
+#print(f"v_count: {v_count}")
 
 #The total number of votes cast
 
